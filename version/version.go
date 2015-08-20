8cff17db30ddda744fecf2fe4361ce661d885b7e
Kim Johansson <kim@detectify.com>
2015-08-20 16:13:44 +0200

    Add generated version description

---
+++ version/version.go
@@ -0,0 +1,7 @@
+package version
+
+// Version indicates which version of the binary is running. This is set to
+// the latest release tag by hand, always suffixed by "+unknown". During
+// build, it will be replaced by the actual version. The value here will be
+// used if the registry is run after a go get based install.
+var Version = "e6c0b8f.m+unknown"
